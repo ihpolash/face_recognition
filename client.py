d2839698e4dc3787ac98b46374d258cc4ad2e319
ihpolash <ismailhossainpolash@gmail.com>
2022-06-22 00:39:23 +0600

    update client.py

--- client.py
+++ client.py
@@ -101,49 +101,49 @@ def face_enrollment(image, name):
     if filesize > 5242880:  # 5 MiB in bytes
         response = {"result": "The maximum file size that can be uploaded is 5 MiB"}
     else:
-        try:
+        # try:
         # change this as you see fit
-            try:
-                with open('face_encoding.pickle', 'rb+') as handle:
-                    load_data = pickle.load(handle)
+        try:
+            with open('face_encoding.pickle', 'rb+') as handle:
+                load_data = pickle.load(handle)
 
-                known_face_encodings = load_data["face_encodings"]
-                known_face_names = load_data["user_names"]
-            except:
-                known_face_encodings = []
-                known_face_names = []
-            
-            image_path = default_storage.save("tmp/temp.jpg", ContentFile(image.read()))
-            tmp_file = os.path.join(settings.MEDIA_ROOT, image_path)
-
-            result, tmp_file = face_check(tmp_file)
-
-            if result['username'] in known_face_names:
-                response = {"result": "User already exists!"}
-            else:
-                # load_image = face_recognition.load_image_file(tmp_file)
-                load_image = cv2.imread(tmp_file)
-                height, width, channels = load_image.shape
-                if height or width > 720:
-                    small_frame = cv2.resize(load_image, (0, 0), fx=0.25, fy=0.25)
-                    load_image = small_frame[:, :, ::-1]
-                print(load_image)
-                
-
-                face_encoding = face_recognition.face_encodings(load_image)[0]
-
-                # Create arrays of known face encodings and their names
-                known_face_encodings.append(face_encoding)
-                known_face_names.append(name)
-
-                store_data = {"face_encodings" : known_face_encodings,"user_names": known_face_names}
-
-                with open(f'face_encoding.pickle', 'wb+') as handle:
-                    pickle.dump(store_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                    
-                response = {"result": "Enrollment Successful"}
-            default_storage.delete(tmp_file)
+            known_face_encodings = load_data["face_encodings"]
+            known_face_names = load_data["user_names"]
         except:
-            response = {"result": "Enrollment Unsuccessful"}
+            known_face_encodings = []
+            known_face_names = []
+        
+        image_path = default_storage.save("tmp/temp.jpg", ContentFile(image.read()))
+        tmp_file = os.path.join(settings.MEDIA_ROOT, image_path)
+
+        result, tmp_file = face_check(tmp_file)
+
+        if result['username'] in known_face_names:
+            response = {"result": "User already exists!"}
+        else:
+            # load_image = face_recognition.load_image_file(tmp_file)
+            load_image = cv2.imread(tmp_file)
+            # height, width, channels = load_image.shape
+            # if height or width > 720:
+            #     small_frame = cv2.resize(load_image, (0, 0), fx=0.25, fy=0.25)
+            #     load_image = small_frame[:, :, ::-1]
+            print(load_image)
+            
+
+            face_encoding = face_recognition.face_encodings(load_image)[0]
+
+            # Create arrays of known face encodings and their names
+            known_face_encodings.append(face_encoding)
+            known_face_names.append(name)
+
+            store_data = {"face_encodings" : known_face_encodings,"user_names": known_face_names}
+
+            with open(f'face_encoding.pickle', 'wb+') as handle:
+                pickle.dump(store_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                
+            response = {"result": "Enrollment Successful"}
+        default_storage.delete(tmp_file)
+        # except:
+        #     response = {"result": "Enrollment Unsuccessful"}
 
     return response
